41fdc462580b882ed4915f023332c2fbe4bce413
Enoch Benjamin <161935736+benjamin-enoch@users.noreply.github.com>
2025-01-29 15:46:55 +0530

    Add files via upload

---
+++ python_1.py
@@ -0,0 +1,30 @@
+#python practices
+'''-----------------------------------------------------------Data types---------------------------------------------------------'''
+
+
+
+x=6.32222222  
+'''----------------------------------------INTEGER (INT)----------------------------------------'''
+y= 9           #Numerical value without Decimals is called as integer.
+print(x,y)
+'''----------------------------------------Float (float)----------------------------------------'''
+print(float(x))              #Numerical value with decimal is known as Float.
+ 
+'''----------------------------------------complex (i+j)----------------------------------------'''
+comp_lex= 5+6j
+print(type(comp_lex)) 
+
+'''----------------------------------------Boolean (bool)----------------------------------------'''
+print(isinstance(comp_lex,complex))
+
+'''We can mark TRUE or FALSE using integers also'''
+print(bool(0))    #This says False
+print(bool(1))    #This says True
+
+
+
+#**********************************************SEQUENTIAL DATA TYPES************************************
+
+#Strings.......
+
+
